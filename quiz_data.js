847698ce3a68d6159e82745a07d6c1552137a6e8
zczlkzh <35736935+zczlkzh@users.noreply.github.com>
2018-02-27 11:31:03 +0000

    Create quiz_data.js

---
+++ quiz_data.js
@@ -0,0 +1,39 @@
+const data = [
+            
+                    //{PplName: "#1 type of Stellar populations" ,  Components: "blue disk stars",          FirstProperty: "metal rich",     SecondProperty: "young and blue", ThirdProperty: "circular orbits"              },
+                    //{PplName: "#2 type of Stellar populations",   Components: "red bulge and halo stars", FirstProperty: "metal poor",     SecondProperty: "old and red",    ThirdProperty: "elliptical and tilted orbits" },
+                    //{Classification: "galaxy classification",     TypeOne: "ellipticals",                 TypeTwo:"dwarf ellipticals",     TypeThree:"spirals",              TypeFour:"barred spirals", TypeFive:"irregulars"},
+                   // {Evidence: "evidence",                        EvidenceOne: "photo-electric effect",   EvidenceTwo:"black-body radiation", EvidenceThree: "compton scattering"},
+                    {Pudding: "pudding", SolutionOne: "Plum pudding"},
+                    {Work: "work", AnswerOne: "work function"},
+                    {Wavelength: "wavelength", OneHundredMetres: "Radiowaves", OneCentimeter: "Microwave", TenThousandNanoMetres: "Infrared", FiveHundredNanoMetres: "Visible Light", OneHundredNanoMetres: "Ultraviolet", OneNanoMetre: "X-ray"},
+                    {Shape: "shape", E0: "round", E7: "elongated"},
+                    {Shift: "shift", Blueshift: "towards", Redshift: "away"},
+                    {Series:"series", Lyman: "1", Balmer: "2", Paschen: "3"},
+                    {OBAFGKMColour:"obafgkmcolour", OClassification: "Blue", BClassification: "Light Blue", AClassification: "White", FClassification: "Yellow-White", GClassification: "Yellow", KClassification: "Orange", MClassification: "Blue"},
+                    {OBAFGKMTemp:"obafgkmtemp", OType: "40000 K", BType: "20000 K", AType: "10000 K", FType: "7500 K", GType: "5500 K", KType: "4000 K", MType: "3000 K"}
+                    
+                    
+                    
+                   ];
+
+
+/* Below is the code that exports the constant named data above. */ 
+                
+exports.data = data;
+
+const data_ans = [
+            
+                    {PplName: "#1 type of Stellar populations" ,  Components: "blue disk stars",          FirstProperty: "metal rich",     SecondProperty: "young and blue", ThirdProperty: "circular orbits"              },
+                    {PplName: "#2 type of Stellar populations",   Components: "red bulge and halo stars", FirstProperty: "metal poor",     SecondProperty: "old and red",    ThirdProperty: "elliptical and tilted orbits" }
+                    
+                   ];
+                   
+exports.data1 = data_ans;
+
+const data_ans2 = [
+            
+                    {Classification: "galaxy classification",     TypeOne: "ellipticals",                 TypeTwo:"dwarf ellipticals",     TypeThree:"spirals",              TypeFour:"barred spirals", TypeFive:"irregulars"}
+                  
+                   ];
+exports.data2 = data_ans2;
